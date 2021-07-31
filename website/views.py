c15e8e37ec9e62979d1baea8f2172936857a6ed8
Steven Ngo <stevenngo97@gmail.com>
2021-07-31 16:28:26 +1000

    first commit

---
+++ website/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from . import data
+from json import dumps
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    return "Homepage"
+
+@views.route('/data')
+def get_data_endpoint():
+    response = data.get_data()
+
+    return response
+    
+
+
+    # try:
+    #     data = get_data()
+    #     return "SUCCESS"
+    # catch Exception:
+    #     return "FAILED TO FETCH" 
